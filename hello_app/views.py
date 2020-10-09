1012da36c6b77d276e0639dd4eeb6d4306b348bf
Nick Thomas <nicktho@microsoft.com>
2020-10-09 12:20:56 -0400

    First commit

---
+++ hello_app/views.py
@@ -0,0 +1,32 @@
+from datetime import datetime
+from flask import Flask, render_template, jsonify
+from . import app
+import json
+
+@app.route("/")
+def idp():
+    return jsonify(
+        {
+            "providers": ["Dev01TestObj11.com","Dev01TestObj22.com","Dev01TestObj33.com"],
+        })
+
+@app.route("/about/")
+def about():
+    return render_template("about.html")
+
+@app.route("/contact/")
+def contact():
+    return render_template("contact.html")
+
+@app.route("/hello/")
+@app.route("/hello/<name>")
+def hello_there(name = None):
+    return render_template(
+        "hello_there.html",
+        name=name,
+        date=datetime.now()
+    )
+
+@app.route("/api/data")
+def get_data():
+    return app.send_static_file("data.json")
